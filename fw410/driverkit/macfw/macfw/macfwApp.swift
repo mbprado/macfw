24da2b160476939f5736b066405910f6587c7712
Murilo Borghi Prado <mborghi@Murilos-Old-MacBook-Pro.local>
2026-08-16 23:47:45 +0200

    Add Xcode DriverKit project skeleton

---
+++ fw410/driverkit/macfw/macfw/macfwApp.swift
@@ -0,0 +1,17 @@
+//
+//  macfwApp.swift
+//  macfw
+//
+//  Created by Murilo Borghi Prado on 16.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct macfwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
