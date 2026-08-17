eec9ee116e465b77a1a2d31f233745d3a7242073
Murilo Borghi Prado <mborghi@Murilos-Old-MacBook-Pro.local>
2026-08-17 10:22:33 +0200

    adding legacy xcode path

---
+++ fw410/driverkit/legacy/macfw/macfwUITests/macfwUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  macfwUITestsLaunchTests.swift
+//  macfwUITests
+//
+//  Created by Murilo Borghi Prado on 17.08.2026.
+//
+
+import XCTest
+
+final class macfwUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
