83bfcb83ef696b834966a1efda2d7336fba79822
Mu Borghi <45686968+mbprado@users.noreply.github.com>
2026-08-25 14:53:44 +0200

    Add verbose transport diagnostics option

--- fw410/tools/transport/haltransport/build_identity.cpp
+++ fw410/tools/transport/haltransport/build_identity.cpp
@@ -1,12 +1,29 @@
 #include "../../../version.h"
 
+#include <crt_externs.h>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 namespace {
 struct BuildIdentityBanner {
     BuildIdentityBanner() {
+        bool verbose = false;
+        const int argc = *_NSGetArgc();
+        char** argv = *_NSGetArgv();
+        for (int i = 1; i < argc; ++i) {
+            if (std::strcmp(argv[i], "--verbose") == 0 ||
+                std::strcmp(argv[i], "--debug") == 0) {
+                verbose = true;
+                break;
+            }
+        }
+        if (verbose) setenv("MACFW_VERBOSE", "1", 1);
+
         std::printf("macfw haltransport %s build %s\n", macfw::build::kVersion,
                     macfw::build::kGitSha);
+        if (verbose)
+            std::printf("verbose transport diagnostics: enabled\n");
     }
 };
 
