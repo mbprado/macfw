04e2ea10c4d7d4584db23d990c3a676382fa637f
Mu Borghi <45686968+mbprado@users.noreply.github.com>
2026-09-01 12:23:05 +0200

    Coalesce live headphone and AUX slider writes

--- fw410/control-panel/Sources/slider_async_extension.mm
+++ fw410/control-panel/Sources/slider_async_extension.mm
@@ -8,29 +8,64 @@
 static NSString *const kMacfwSliderControlTool = @"/Library/Application Support/macfw/fw410/tools/control/fw410ctl/fw410ctl";
 
 @interface AppDelegate : NSObject
+- (void)volumeSliderMoved:(NSSlider *)sender;
+- (void)auxStreamSliderMoved:(NSSlider *)sender;
+- (void)auxOutputSliderMoved:(NSSlider *)sender;
 - (void)commitStereoControl:(NSString *)control left:(NSSlider *)left right:(NSSlider *)right;
 @end
+
+static void MacfwQueueStereo(id self, NSString *control, NSString *leftKey, NSString *rightKey) {
+    NSSlider *left = [self valueForKey:leftKey];
+    NSSlider *right = [self valueForKey:rightKey];
+    if (!left || !right) return;
+    MacfwQueueControlWrite(kMacfwSliderControlTool,
+                           control,
+                           @[control, @"set",
+                             [NSString stringWithFormat:@"%d", (int)llround(left.doubleValue)],
+                             [NSString stringWithFormat:@"%d", (int)llround(right.doubleValue)]]);
+}
 
 @implementation AppDelegate (MacfwAsyncSliders)
 
 + (void)load {
     Class cls = NSClassFromString(@"AppDelegate");
     if (!cls) return;
-    Method original = class_getInstanceMethod(cls, @selector(commitStereoControl:left:right:));
-    Method replacement = class_getInstanceMethod(cls, @selector(macfw_commitStereoControl:left:right:));
-    if (original && replacement) method_exchangeImplementations(original, replacement);
+    struct { SEL original; SEL replacement; } swaps[] = {
+        {@selector(volumeSliderMoved:), @selector(macfw_liveVolumeSliderMoved:)},
+        {@selector(auxStreamSliderMoved:), @selector(macfw_liveAuxStreamSliderMoved:)},
+        {@selector(auxOutputSliderMoved:), @selector(macfw_liveAuxOutputSliderMoved:)},
+        {@selector(commitStereoControl:left:right:), @selector(macfw_commitStereoControl:left:right:)},
+    };
+    for (const auto &swap : swaps) {
+        Method original = class_getInstanceMethod(cls, swap.original);
+        Method replacement = class_getInstanceMethod(cls, swap.replacement);
+        if (original && replacement) method_exchangeImplementations(original, replacement);
+    }
+}
+
+- (void)macfw_liveVolumeSliderMoved:(NSSlider *)sender {
+    [self macfw_liveVolumeSliderMoved:sender];
+    MacfwQueueStereo(self, @"headphone-volume", @"leftSlider", @"rightSlider");
+}
+
+- (void)macfw_liveAuxStreamSliderMoved:(NSSlider *)sender {
+    [self macfw_liveAuxStreamSliderMoved:sender];
+    MacfwQueueStereo(self, @"aux-stream12-volume", @"auxStreamLeftSlider", @"auxStreamRightSlider");
+}
+
+- (void)macfw_liveAuxOutputSliderMoved:(NSSlider *)sender {
+    [self macfw_liveAuxOutputSliderMoved:sender];
+    MacfwQueueStereo(self, @"aux-output-volume", @"auxOutputLeftSlider", @"auxOutputRightSlider");
 }
 
 - (void)macfw_commitStereoControl:(NSString *)control
                              left:(NSSlider *)left
                             right:(NSSlider *)right {
-    const int leftDb = (int)llround(left.doubleValue);
-    const int rightDb = (int)llround(right.doubleValue);
     MacfwQueueControlWrite(kMacfwSliderControlTool,
                            control,
                            @[control, @"set",
-                             [NSString stringWithFormat:@"%d", leftDb],
-                             [NSString stringWithFormat:@"%d", rightDb]]);
+                             [NSString stringWithFormat:@"%d", (int)llround(left.doubleValue)],
+                             [NSString stringWithFormat:@"%d", (int)llround(right.doubleValue)]]);
 }
 
 @end
